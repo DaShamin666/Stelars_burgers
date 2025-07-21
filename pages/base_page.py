bd57d373ebf56c9cc62156ca00e7c1aea74f6f68
a123 <samindaniil97@gmail.com>
2025-07-21 19:04:06 +0300

    TASK-263

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,5 +1,13 @@
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from pages.registration_page import RegistrationPage
+from pages.sign_in_button_in_registration import SignInRegistrationPage
+from pages.sign_in_profile_button import SignIn
+from pages.sign_in_password_recovery import SignInPasswordRecoveryPage
+from pages.passage import Passage
+from pages.sign_in_to_your_account import LogInToYourAccountPage
+from pages.exit_in_profile import ExitInProfile
+from pages.buns_sauces_fillings_passages import BunsSauceFillingsPassagesPage
 import allure
 
 class BasePage:
@@ -8,6 +16,14 @@ class BasePage:
     def __init__(self, driver):
         self.driver = driver
         self.wait = WebDriverWait(driver, 10)
+        self.registration_page = RegistrationPage(self.driver)
+        self.sign_in = SignIn(self.driver)
+        self.sign_in_profile_registration = SignInRegistrationPage(self.driver)
+        self.sign_in_password_recovery = SignInPasswordRecoveryPage(self.driver)
+        self.log_in_to_your_account = LogInToYourAccountPage(self.driver)
+        self.passage = Passage(self.driver)
+        self.exit_in_profile = ExitInProfile(self.driver)
+        self.buns_passages = BunsSauceFillingsPassagesPage(self.driver)
 
     def find_element(self, locator):
         """Найти элемент с явным ожиданием"""
